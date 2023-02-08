996da541e444a720679c5a68e673e6d357297739
RandyHucker <rhucker@physna.com>
2023-02-08 14:17:32 -0500

    Lab4

---
+++ Lab4/Distance.cpp
@@ -0,0 +1,143 @@
+#include "Distance.h"
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// Default constructor
+Distance::Distance()
+{
+    Meters = 0.0;
+    Feet = 0;
+    Inches = 0;
+}
+
+// Fill constructor for imperial units
+Distance::Distance(double f, double i)
+{
+    Feet = f;
+    Inches = i;
+    Meters = 0;
+    if (i > 12)
+    {
+        Feet += floor(i / 12);
+        Inches = Inches - (Feet * 12);
+    }
+    UpdateMetric();
+}
+
+// Fill constructor for metric units
+Distance::Distance(double m)
+{
+    Meters = m;
+    Inches = 0;
+    Feet = 0;
+    UpdateImperial();
+}
+
+// Destructor
+Distance::~Distance()
+{
+}
+
+// Getters
+double Distance::getInches()
+{
+    return (Inches);
+}
+
+double Distance::getFeet()
+{
+    return (Feet);
+}
+double Distance::getMeters()
+{
+    return (Meters);
+}
+
+// Setters
+void Distance::setInches(double i)
+{
+    Inches = i;
+    if (i > 12)
+    {
+        Feet += floor(i / 12);
+        Inches = Inches - (Feet * 12);
+    }
+    UpdateMetric();
+}
+
+void Distance::setFeet(double f)
+{
+    Feet = f;
+    UpdateMetric();
+}
+
+void Distance::setMeters(double m)
+{
+    Meters = m;
+    UpdateImperial();
+}
+
+// Additional Member Functions
+
+void Distance::UpdateImperial() // updates the feet and inches according to the current value of meters
+{
+    Inches = 39.3701 * Meters;
+    Feet = floor(Inches / 12);
+    Inches = Inches - (Feet * 12);
+}
+
+void Distance::UpdateMetric() // updates meters according to the current value of feet and inches
+{
+    Meters = (Inches + (Feet * 12)) * 0.0254;
+}
+
+void Distance::print() // uses cout to disply the values of class attributes
+{
+    cout << fixed << setprecision(2);
+    cout << endl;
+    cout << "Meters: " << Meters << endl;
+    cout << "Feet: " << Feet << endl;
+    cout << "Inches: " << Inches << endl;
+    cout << endl;
+}
+
+// Operator Overloads
+
+void Distance::operator+(Distance &D)
+{
+    Meters += D.getMeters();
+    UpdateImperial();
+}
+
+void Distance::operator-(Distance &D)
+{
+    this->Meters -= D.Meters;
+    this->UpdateImperial();
+}
+
+void Distance::operator*(Distance &D)
+{
+    this->Meters *= D.Meters;
+    this->UpdateImperial();
+}
+
+void Distance::operator/(Distance &D)
+{
+    this->Meters /= D.Meters;
+    this->UpdateImperial();
+}
+
+bool Distance::operator==(Distance &D)
+{
+    return (this->Meters == D.Meters || (this->Inches == D.Inches && this->Feet == D.Inches));
+}
+
+std::ostream &operator<<(ostream &os, Distance &D)
+{
+    os << to_string(D.getMeters()) << " meters / " << to_string(D.getFeet()) + " feet " << to_string(D.getInches()) << " inches ";
+    return os;
+}
---
+++ Lab4/Task4.cpp
@@ -0,0 +1,97 @@
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "Distance.h"
+
+using namespace std;
+
+void showOperatorMenu()
+{
+    cout << "Which operation would you like to test? (Enter a number 0-5):" << endl;
+    cout << "(0) Addition (+)" << endl;
+    cout << "(1) Subtraction (-)" << endl;
+    cout << "(2) Multiplication (*)" << endl;
+    cout << "(3) Division (/)" << endl;
+    cout << "(4) to_string (str)" << endl;
+    cout << "(5) Equivalence (==)" << endl;
+}
+
+int main()
+{
+    int ans = 1;
+    double x1 = 0;
+    double x2 = 0;
+    int operation = 0;
+
+    cout << "All calculations are performed according to the value stored for meters." << endl;
+    cout << "Because of this, only an x value is needed for input (instead of x for feet and y for inches)" << endl;
+    cout << "Feet and inch value are adjusted according to the value for meters." << endl;
+    cout << endl;
+    cout << "Please enter x for the first distance (in meters): ";
+    cin >> x1;
+    cout << endl;
+    // while (!isdigit(x1))
+    // {
+    //     cout << "Please enter a numerical x: ";
+    //     cin >> x1;
+    // }
+
+    Distance D1 = Distance(x1);
+    while (ans == 1)
+    {
+        showOperatorMenu();
+
+        cin >> operation;
+        while (!0 && !1 && !2 && !3 && !4 && !5)
+        {
+            cout << "Please enter a numerical option: ";
+            cin >> operation;
+        }
+
+        Distance D2 = Distance();
+
+        if (operation != 4)
+        {
+            cout << "Please enter x for the second distance (in meters): ";
+            cin >> x2;
+            cout << endl;
+            // while (!isdigit(x2))
+            // {
+            //     cout << "Please enter a numerical x: ";
+            //     cin >> x2;
+            // }
+            D2.setMeters(x2);
+        }
+
+        switch (operation)
+        {
+        case 0:
+            D1 + D2;
+            break;
+        case 1:
+            D1 - D2;
+            break;
+        case 2:
+            D1 *D2;
+            break;
+        case 3:
+            D1 / D2;
+            break;
+        case 4:
+            cout << D1;
+            break;
+        case 5:
+            string s = (D1 == D2) ? "True" : "False";
+            cout << s << endl;
+            break;
+        }
+
+        D1.print();
+
+        cout << "Would you like to test another operation? (Enter '1' for Yes, anything else for No): ";
+        cin >> ans;
+    }
+
+    return 0;
+}
