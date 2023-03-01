c21e934f35d10e0ebbac9f386d58d75b38d1ad7e
RandyHucker <rhucker@physna.com>
2023-03-01 14:37:07 -0500

    Lab7 Initial

---
+++ Lab7/Task2/QueueExceptions.h
@@ -0,0 +1,22 @@
+#include <exception>
+#include <string>
+
+using namespace std;
+
+class QueueOverflowException : public std::exception
+{
+public:
+    string what() // overrides what() virtual function from exception that returns an explanatory string
+    {
+        return "The Queue is Full! You cannot add something to a full Queue.";
+    }
+};
+
+class QueueUnderFlowException : public std::exception
+{
+public:
+    string what() // overrides what() virtual function from exception that returns an explanatory string
+    {
+        return "The Queue is Empty! You cannot remove something from an empty Queue / there is no top item to peek at.";
+    }
+};
